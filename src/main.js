5e8dd56443d0bba483d177c4f6543e17e86bdbc2
aposte7 <olman4jesus@gmail.com>
2025-07-15 12:20:17 +0300

    feat: implement previous and next button functionalities

--- src/main.js
+++ src/main.js
@@ -1,5 +1,16 @@
 import './style.css'
 
+const flashCards = [
+	{ img: 'src/assets/images/ketone.jpg', label: 'Ketone' },
+	{ img: 'src/assets/images/Ester.jpg', label: 'Ester' },
+	{ img: 'src/assets/images/Carboxylic Acid.jpg', label: 'Carboxylic Acid' },
+	{ img: 'src/assets/images/Amine.jpg', label: 'Amine' },
+	{ img: 'src/assets/images/Alkyl halide.jpg', label: 'Alkyl Halide' },
+	{ img: 'src/assets/images/Aldehyde.jpg', label: 'Aldehyde' },
+	{ img: 'src/assets/images/acohol.jpg', label: 'Alcohol' },
+	{ img: 'src/assets/images/pentane.webp', label: 'Pentane' },
+]
+
 document.querySelector('#app').innerHTML = `
  <div class="h-dvh">
   <div class="bg-white w-[48rem] mx-auto h-full flex flex-col items-center justify-center">
@@ -9,8 +20,8 @@ document.querySelector('#app').innerHTML = `
           <div>⭐</div>
         </div>
 
-        <div class="flex h-full items-center justify-center">
-          <img src="" alt="" class="bg-gray-400 h-44 w-56" />
+        <div id="text_container" class="flex h-full items-center justify-center">
+        
         </div>
       </div>
 
@@ -20,13 +31,13 @@ document.querySelector('#app').innerHTML = `
         </button>
       
         <div class="flex gap-4 items-center">
-          <button>
+          <button id="prev_btn">
             <svg xmlns="http://www.w3.org/2000/svg" width="45" height="45" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-circle-arrow-left-icon lucide-circle-arrow-left"><circle cx="12" cy="12" r="10"/><path d="m12 8-4 4 4 4"/><path d="M16 12H8"/></svg>
           </button>
 
           <p class="text-xl">1/65</p>
 
-          <button class="">
+          <button id="next_btn" class="">
             <svg xmlns="http://www.w3.org/2000/svg" width="45" height="45" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-circle-arrow-right-icon lucide-circle-arrow-right"><circle cx="12" cy="12" r="10"/><path d="m12 16 4-4-4-4"/><path d="M8 12h8"/></svg>
           </button>
 
@@ -41,4 +52,59 @@ document.querySelector('#app').innerHTML = `
   </div>
 `
 
-setupCounter(document.querySelector('#counter'))
+const textContainer = document.querySelector('#text_container')
+const answerParagraph = document.createElement('p')
+const image = document.createElement('img')
+const prevBtn = document.querySelector('#prev_btn')
+const nextBtn = document.querySelector('#next_btn')
+
+let index = 0
+
+function containerLoader() {
+	updateContent()
+
+	prevBtn.onclick = () => {
+		if (index > 0) {
+			index -= 1
+			updateContent()
+		}
+	}
+
+	nextBtn.onclick = () => {
+		if (index < flashCards.length - 1) {
+			index += 1
+			updateContent()
+		}
+	}
+}
+
+function updateContent() {
+	image.classList.add('h-fit', 'object-fit', 'w-60', 'bg-gray-400')
+	image.src = flashCards[index].img
+
+	answerParagraph.classList.add('text-2xl')
+	answerParagraph.textContent = flashCards[index].label
+
+	textContainer.insertAdjacentElement('afterbegin', image)
+
+	if (index <= 0) {
+		prevBtn.setAttribute('disabled', '')
+		prevBtn.classList.add('text-gray-400')
+		prevBtn.classList.remove('text-gray-700')
+	} else {
+		prevBtn.removeAttribute('disabled')
+		prevBtn.classList.remove('text-gray-400')
+		prevBtn.classList.add('text-gray-700')
+	}
+
+	if (index >= flashCards.length - 1) {
+		nextBtn.setAttribute('disabled', true)
+		nextBtn.classList.add('text-gray-400')
+		nextBtn.classList.remove('text-gray-700')
+	} else {
+		nextBtn.removeAttribute('disabled')
+		nextBtn.classList.remove('text-gray-400')
+		nextBtn.classList.add('text-gray-700')
+	}
+}
+containerLoader()
